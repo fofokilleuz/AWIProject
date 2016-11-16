558427b11248ca7062acf43efc92291cd23e02c8
jeremybeule <jeremy.beule@live.fr>
2016-11-16 10:35:13 +0100

    Ajout des class : LineShoppingCart et ShoppingCart

---
+++ app/model/LineShoppingCart.java
@@ -0,0 +1,67 @@
+package model;
+
+
+import java.util.*;
+
+import javax.persistence.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+import com.avaje.ebean.Model;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import play.api.libs.Codecs;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Product;
+
+	@Entity
+	public class LineShoppingCart extends Model {
+	    
+	    @Id
+	    @GeneratedValue
+	    @Column(name = "id", updatable = false, nullable = false)
+	    public long id;
+	    
+	    @Constraints.Required
+	    public int quantity;
+	    
+	    @Constraints.Required
+	    public long sellPrice;
+	    
+	    @ManyToMany(cascade=CascadeType.ALL)
+	    public Product product;
+	    
+	 
+	    public LineShoppingCart(int quantity, long sellPrice, Product product) {
+			super();
+			this.product=product;
+			this.quantity=quantity;
+			this.sellPrice=sellPrice;
+    
+		}
+		
+	   public static Finder<Long, LineShoppingCart> find = new Finder<Long,LineShoppingCart>(LineShoppingCart.class);
+	    
+	   public void setQuantity(int quantity){
+	        this.quantity = quantity;
+	    }
+	    
+	    public void setSellePrice(long sellPrice){
+	        this.sellPrice=sellPrice;
+	    }
+	    
+	    public void setProduct(Product product){
+	        this.product=product;
+	    }
+	    
+	    public static LineShoppingCart getLineShoppingCartId(Long id)
+	    {
+	        return LineShoppingCart.find.byId(id);
+	    }
+	    
+	}
---
+++ app/model/ShoppingCart.java
@@ -0,0 +1,52 @@
+package model;
+
+
+import java.util.*;
+
+import javax.persistence.*;
+import play.db.ebean.*;
+import com.avaje.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+import com.avaje.ebean.Model;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import play.api.libs.Codecs;
+
+import model.LineShoppingCart;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Product;
+
+	@Entity
+	public class ShoppingCart extends Model {
+	    
+	    @Id
+	    @GeneratedValue
+	    @Column(name = "id", updatable = false, nullable = false)
+	    public long id;
+	    
+	    
+	    @OneToMany(cascade=CascadeType.ALL)
+	    public List<LineShoppingCart> lineShoppingCarts = new ArrayList<LineShoppingCart>();
+	    
+	 
+	    public ShoppingCart() {
+			super();
+		}
+		
+	   public static Finder<Long, ShoppingCart> find = new Finder<Long,ShoppingCart>(ShoppingCart.class);
+	    
+	   public void addLineShoppingCart(LineShoppingCart lineShoppingCart){
+	        this.lineShoppingCarts.add(lineShoppingCart);
+	    }
+	  
+	    public static ShoppingCart getShoppingCartId(Long id)
+	    {
+	        return ShoppingCart.find.byId(id);
+	    }
+	    
+	}
